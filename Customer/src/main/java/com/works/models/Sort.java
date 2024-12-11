adc8c243b5cd591b10678c70606253185c41de9a
Hakan ÖZER <hakanozer02@gmail.com>
2024-12-11 11:56:49 +0300

    Customer

---
+++ Customer/src/main/java/com/works/models/Sort.java
@@ -0,0 +1,16 @@
+package com.works.models;
+
+public class Sort {
+    private boolean empty;
+    private boolean sorted;
+    private boolean unsorted;
+
+    public boolean getEmpty() { return empty; }
+    public void setEmpty(boolean value) { this.empty = value; }
+
+    public boolean getSorted() { return sorted; }
+    public void setSorted(boolean value) { this.sorted = value; }
+
+    public boolean getUnsorted() { return unsorted; }
+    public void setUnsorted(boolean value) { this.unsorted = value; }
+}
